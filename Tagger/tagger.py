7cd0775cfb8b9f554da765dc826652d1a5149ce8
liuhaotian9420 <liuhaotian@heywhale.com>
2021-05-17 11:13:22 +0800

    create for notebook tagging

---
+++ Tagger/tagger.py
@@ -0,0 +1,7 @@
+import sys
+import os
+import json
+sys.path.append('C:\\Users\\Haotian Liu\\Documents\\GitHub\\NotebookAnalyzer\\')
+from Analyzer import Notebook
+from Analyzer import NotebookAnalyzer
+
